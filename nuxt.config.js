b18f8546a77b9c2d7ab0d8822709d6c6ae44b318
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2020-04-28 06:39:55 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -43,6 +43,7 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    './codesandbox'
   ],
   /*
   ** Axios module configuration
